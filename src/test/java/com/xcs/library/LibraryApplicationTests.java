abc5e0540deb70bde865602599e2c5881d67b693
Mack-Jack-2-Dijon <117633879+Mack-Jack-2-Dijon@users.noreply.github.com>
2023-03-20 14:57:40 +0100

    Add files via upload

---
+++ src/test/java/com/xcs/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcs.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
